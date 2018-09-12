b72d4d4817794454bccfa29e35a77c3503c18d2f
jerry <jerry.yi@zhiyuncrm.com>
2018-09-12 20:19:13 +0800

    fix issue

--- LotteryApp/Lottery.Test/Program.cs
+++ LotteryApp/Lottery.Test/Program.cs
@@ -35,7 +35,7 @@ namespace Lottery.Test
                          Console.Title = v;
                      }
                  }
-            };
+            }；
             Timer timer = new Timer(StartBet, p, start.Second < 20 ? (20 - start.Second) * 1000 : (80 - start.Second) * 1000, 60000);
 
             Console.WriteLine("服务已运行");
