067c1c92b58b26dd5095b836d305551c5d5a66a9
yilong <yilong@mallcoo.cn>
2018-09-29 22:22:59 +0800

    fix issue

--- LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
+++ LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
@@ -304,7 +304,7 @@ namespace Lottery.Core.Algorithm
             var query = from p in FactorDic[FactorTypeEnum.AdjacentNumber]
                         join q in FactorDic[FactorTypeEnum.AllPairs]
                            on p.Key equals q.Key
-                        where p.Value.LastInterval <= q.Value.LastInterval && p.Value.LastInterval <=5
+                        where p.Value.LastInterval <= q.Value.LastInterval && p.Value.LastInterval <=3
                         orderby q.Value.LastInterval descending, q.Value.OccurCount descending
                         select p.Key;
             return Build(query);
