5115922fb0bcafb72b4591490d2ec054a6eedaca
jerry <jerry.yi@zhiyuncrm.com>
2018-11-28 15:49:01 +0800

    refine single

--- LotteryApp/Lottery.App/MainWindow.xaml.cs
+++ LotteryApp/Lottery.App/MainWindow.xaml.cs
@@ -23,46 +23,49 @@ namespace Lottery.App
             int takeNumber = int.Parse(ConfigurationManager.AppSettings["GameNumber"]);
             string lotteryName = ConfigurationManager.AppSettings["LotteryName"];
             string[] gameArgs = new string[] { "1", "2" };
-            Dynamic23[] singles = gameArgs.Select((c, i) => new Dynamic23
+            //Dynamic23[] singles = gameArgs.Select((c, i) => new Dynamic23
+            //{
+            //    EnableSinglePattern = false,
+            //    BetIndex = 0,
+            //    LastBet = null,
+            //    BetCycle = 5,
+            //    Number = 1,
+            //    TakeNumber = takeNumber,
+            //    GameName = "single",
+            //    GameArgs = string.Join("." , "all", c),
+            //    LotteryName = lotteryName,
+            //    Dispatcher = (u, v) => UpdateUI(string.Join(".", "single", c), u, v)
+            //}).ToArray();
+
+            int[] cycles = new int[] { 15, 50, 100 };
+            Dynamic23[] longSingles = cycles.SelectMany((c, i) => new Dynamic23[]
             {
-                EnableSinglePattern = false,
-                BetIndex = 0,
-                LastBet = null,
-                BetCycle = 5,
-                Number = 1,
-                TakeNumber = takeNumber,
-                GameName = "single",
-                GameArgs = string.Join("." , "all", c),
-                LotteryName = lotteryName,
-                Dispatcher = (u, v) => UpdateUI(string.Join(".", "single", c), u, v)
+                new Dynamic23
+                {
+                    BetIndex = 0,
+                    LastBet = null,
+                    Number = 1,
+                    TakeNumber = c,
+                    GameName = "single",
+                    GameArgs =  "after",
+                    LotteryName = lotteryName,
+                    Dispatcher = (u, v) => UpdateUI(string.Join(".", "single", c), u, v)
+                },
+                new Dynamic23
+                {
+                    BetIndex = 0,
+                    LastBet = null,
+                    Number = 1,
+                    WaitInterval =3,
+                    TakeNumber = c,
+                    GameName = "single",
+                    GameArgs = "after",
+                    LotteryName = lotteryName,
+                    Dispatcher = (u, v) => UpdateUI(string.Join(".", "single", c, 3), u, v)
+                }
             }).ToArray();
 
-            gameArgs = new string[] { "0", "1" };
-            Dynamic23[] longSingles = gameArgs.Select((c, i) => new Dynamic23
-            {
-                BetIndex = 0,
-                LastBet = null,
-                Number = 1,
-                TakeNumber = 50,
-                GameName = "single",
-                GameArgs = string.Join(".", "after", c),
-                LotteryName = lotteryName,
-                Dispatcher = (u, v) => UpdateUI(string.Join(".", "longSingle", c), u, v)
-            }).ToArray();
-
-            Dynamic23[] tuples = gameArgs.Select((c, i) => new Dynamic23
-            {
-                BetIndex = 0,
-                LastBet = null,
-                Number = 2,
-                TakeNumber = takeNumber,
-                GameName = "tuple",
-                GameArgs = string.Join(".", "after", c),
-                LotteryName = lotteryName,
-                Dispatcher = (u, v) => UpdateUI(string.Join(".", "tuple", c), u, v)
-            }).ToArray();
-
-            Dictionary<string, IPlan> dic = singles.Concat(longSingles).Concat(tuples).OfType<IPlan>().ToDictionary(c => c.GetKey(), c => c);
+            Dictionary<string, IPlan> dic = longSingles.OfType<IPlan>().ToDictionary(c => c.GetKey(), c => c);
             PlanInvoker.Current.Init(dic);
         }
 
@@ -74,27 +77,27 @@ namespace Lottery.App
                 System.Windows.Forms.RichTextBox valueBox = null;
                 switch (code)
                 {
-                    case "single.1":
+                    case "single.15":
                         descBox = this.txtFrontDesc;
                         valueBox = this.txtFrontHost.Child as System.Windows.Forms.RichTextBox;
                         break;
-                    case "single.2":
+                    case "single.50":
                         descBox = this.txtMiddleDesc;
                         valueBox = this.txtMiddleHost.Child as System.Windows.Forms.RichTextBox;
                         break;
-                    case "longSingle.0":
+                    case "single.100":
                         descBox = this.txtAfterDesc;
                         valueBox = this.txtAfterHost.Child as System.Windows.Forms.RichTextBox;
                         break;
-                    case "longSingle.1":
+                    case "single.15.3":
                         descBox = this.txtOneAwardDesc;
                         valueBox = this.txtOneAwardHost.Child as System.Windows.Forms.RichTextBox;
                         break;
-                    case "tuple.0":
+                    case "single.50.3":
                         descBox = this.txtFiveDesc;
                         valueBox = this.txtFiveHost.Child as System.Windows.Forms.RichTextBox;
                         break;
-                    case "tuple.1":
+                    case "single.100.3":
                         descBox = this.txtFiftyDesc;
                         valueBox = this.txtFiftyHost.Child as System.Windows.Forms.RichTextBox;
                         break;
