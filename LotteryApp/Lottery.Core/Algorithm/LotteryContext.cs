4fbc54459f6fa7489b93f4bab9c0dcb56e10e496
jerry <jerry.yi@zhiyuncrm.com>
2019-01-28 13:25:59 +0800

    fix issue

--- LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
+++ LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
@@ -532,7 +532,7 @@ namespace Lottery.Core.Algorithm
             var query = from p in FactorDic[r]
                         let values = p.Key.ToString().Select(c => int.Parse(c.ToString())).Skip(1).ToArray()
                         where (InputOption.EnableContinuous ? continuous.Contains(p.Key) : true) && validAwards.Intersect(values).Count() == values.Length
-                        orderby p.Value.MaxInterval, p.Value.OccurCount descending, p.Value.FailureCount, p.Value.LastInterval
+                        orderby p.Value.OccurCount descending, p.Value.MaxInterval, p.Value.FailureCount, p.Value.LastInterval
                         select p.Key;
 
             if (InputOption.UseGeneralTrend && InputOption.GeneralTrendInterval > 0)
