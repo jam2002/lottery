6d7de0e58b7a34cbfc0b7bb054d89c55418c75a6
jerry <jerry.yi@zhiyuncrm.com>
2019-03-13 16:24:26 +0800

    fix history

--- LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
+++ LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
@@ -242,7 +242,7 @@ namespace Lottery.Core.Algorithm
             var query = from p in FactorDic[r]
                         let values = p.Key.ToString().Select(c => int.Parse(c.ToString())).Skip(1).ToArray()
                         where validAwards.Intersect(values).Count() == values.Length && p.Value.OccurCount >= 8 && p.Value.LastInterval < 7
-                        orderby p.Value.OccurCount descending, p.Value.LastInterval descending
+                        orderby p.Value.LastInterval descending, p.Value.OccurCount descending, p.Value.MaxInterval
                         select p.Key;
             return Build(query, r);
         }
