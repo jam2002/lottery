b06b4572d407f18d20e0e317ab16dde6399950cc
jerry <jerry.yi@zhiyuncrm.com>
2019-03-27 19:02:20 +0800

    fix issue

--- LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
+++ LotteryApp/Lottery.Core/Algorithm/LotteryContext.cs
@@ -678,11 +678,22 @@ namespace Lottery.Core.Algorithm
             int[] continuous = InputOption.TupleLength == 4 ? new int[] { 10123, 11234, 12345, 13456, 14567, 15678, 16789, 10789, 10189, 10129 } : new int[] { 1012, 1123, 1234, 1345, 1456, 1567, 1678, 1789, 1089, 1019 };
             int[] validAwards = FactorDic[FactorTypeEnum.Award].Where(c => CheckInterval(c.Value.HitIntervals)).Select(c => c.Key).ToArray();
 
-            var query = from p in FactorDic[r]
-                        let values = p.Key.ToString().Select(c => int.Parse(c.ToString())).Skip(1).ToArray()
-                        where InputOption.EnableContinuous ? continuous.Contains(p.Key) : true
-                        orderby p.Value.OccurCount descending, p.Value.MaxInterval, p.Value.LastInterval descending
-                        select p.Key;
+            IEnumerable<int> query = from p in FactorDic[r]
+                                     let values = p.Key.ToString().Select(c => int.Parse(c.ToString())).Skip(1).ToArray()
+                                     where InputOption.EnableContinuous ? continuous.Contains(p.Key) : true
+                                     orderby p.Value.OccurCount descending, p.Value.MaxInterval, p.Value.LastInterval descending
+                                     select p.Key;
+
+            if (InputOption.UseGeneralTrend && InputOption.LotteryName == "tsssc")
+            {
+                query = query.ToArray().Take(3);
+                query = FactorDic[tupleDic[gameArgs]].Where(c => query.Contains(c.Key))
+                                                                         .OrderByDescending(c => c.Value.OccurCount)
+                                                                         .ThenBy(c => c.Value.MaxInterval)
+                                                                         .ThenByDescending(c => c.Value.LastInterval)
+                                                                         .Select(c => c.Key)
+                                                                         .ToArray();
+            }
 
             if (InputOption.UseGeneralTrend && InputOption.GeneralTrendInterval > 0)
             {
