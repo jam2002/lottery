0876377edda717ebc8110aaf4183bd76ca695225
Jerry Yi <long.yi@bizatmobile.com>
2019-05-10 14:39:34 +0800

    fix txssc source

--- LotteryApp/Lottery.Core/Algorithm/Calculator.cs
+++ LotteryApp/Lottery.Core/Algorithm/Calculator.cs
@@ -125,11 +125,12 @@ namespace Lottery.Core.Algorithm
                 {
                     try
                     {
-                        lotteries = GetTsNumbers("https://www.pp926.com/api/lastOpenedIssues.php?id=1&issueCount=200");
+						lotteries = GetTsNumbers("http://tx-ssc.com/api/getData");
+                       
                     }
                     catch
                     {
-                        lotteries = GetTsNumbers("http://tx-ssc.com/api/getData");
+                        lotteries = GetTsNumbers("https://www.pp926.com/api/lastOpenedIssues.php?id=1&issueCount=200");
                     }
                 }
                 else if (lottery.Source == 4)
